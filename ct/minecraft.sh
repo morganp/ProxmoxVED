86b80a74192486b94eb80386b36a630a64cd4eac
Morgan Prior <36314+morganp@users.noreply.github.com>
2026-02-28 18:55:42 +0000

    Add ct/minecraft.sh - Minecraft Java LXC container

---
+++ ct/minecraft.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2026 community-scripts ORG
+# Author: morganp
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://www.minecraft.net/
+
+APP="Minecraft"
+var_tags="${var_tags:-gaming;java}"
+var_cpu="${var_cpu:-2}"
+var_ram="${var_ram:-2048}"
+var_disk="${var_disk:-8}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-13}"
+var_unprivileged="${var_unprivileged:-1}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+  if [[ ! -f /usr/bin/java ]]; then
+    msg_error "No Java Installation Found!"
+    exit
+  fi
+
+  JAVA_VERSION="21" setup_java
+  msg_ok "Updated Java successfully!"
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Java is installed and ready at:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}ssh root@${IP}${CL}"
